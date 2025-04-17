fba5694b858dc70a8e4f5da62a6a243d8ac89bcf
Frontachini <frontbusinessinq@gmail.com>
2025-04-17 19:29:25 +0200

    finished

---
+++ script.js
@@ -0,0 +1,22 @@
+const checkBtn = document.getElementById("check-btn");
+const result = document.querySelector(".result");
+
+
+checkBtn.addEventListener('click', checkInput)
+
+
+function checkInput(){
+    const userInput = document.getElementById("user-input").value.toLowerCase();
+    const userInputBackwards = userInput.split('').reverse().join('');
+
+    console.log(userInputBackwards);
+
+    if(userInput == userInputBackwards.toLowerCase()){
+        result.innerHTML = `<p>Yes, <span>'${userInput}'</span> is a palindrome!</p>`
+    }else{
+        result.innerHTML = `<p>Nope, <span>'${userInput}'</span> is not a palindrome!</p>`
+    }
+
+
+    
+}
